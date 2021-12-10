a74dcecf5e69ec2008193761a9b137bf0ddd40d2
Bruno Bergamaschi <brunobergamaschi@live.com>
2021-12-10 18:40:38 -0300

    desafio concluído

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,61 +7,89 @@ import { formatPrice } from '../../util/format';
 import { useCart } from '../../hooks/useCart';
 
 interface Product {
-  id: number;
-  title: string;
-  price: number;
-  image: string;
+	id: number;
+	title: string;
+	price: number;
+	image: string;
 }
 
 interface ProductFormatted extends Product {
-  priceFormatted: string;
+	priceFormatted: string;
 }
 
 interface CartItemsAmount {
-  [key: number]: number;
+	[key: number]: number;
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+	const [products, setProducts] = useState<ProductFormatted[]>([]);
+	const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+	console.log(cart);
 
-  useEffect(() => {
-    async function loadProducts() {
-      // TODO
-    }
+	const cartItemsAmount = cart.reduce(
+		(sumAmount, product) => {
+			if (product.id === 1) {
+				sumAmount[1] += product.amount;
+			} else if (product.id === 2) {
+				sumAmount[2] += product.amount;
+			} else if (product.id === 3) {
+				sumAmount[3] += product.amount;
+			} else if (product.id === 4) {
+				sumAmount[4] += product.amount;
+			} else if (product.id === 5) {
+				sumAmount[5] += product.amount;
+			} else if (product.id === 6) {
+				sumAmount[6] += product.amount;
+			}
 
-    loadProducts();
-  }, []);
+			return sumAmount;
+		},
+		{
+			1: 0,
+			2: 0,
+			3: 0,
+			4: 0,
+			5: 0,
+			6: 0,
+		} as CartItemsAmount
+	);
 
-  function handleAddProduct(id: number) {
-    // TODO
-  }
+	useEffect(() => {
+		async function loadProducts() {
+			await api.get('products').then((response) => setProducts(response.data));
+		}
 
-  return (
-    <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
+		loadProducts();
+	}, []);
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+	function handleAddProduct(id: number) {
+		addProduct(id);
+	}
+
+	return (
+		<ProductList>
+			{products.map((product) => (
+				<li key={product.id}>
+					<img src={product.image} alt={product.title} />
+					<strong>{product.title}</strong>
+					<span>R$ {product.price}</span>
+					<button
+						type='button'
+						data-testid='add-product-button'
+						onClick={() => handleAddProduct(product.id)}
+					>
+						<div data-testid='cart-product-quantity'>
+							<MdAddShoppingCart size={16} color='#FFF' />
+							{cartItemsAmount[product.id] || 0}
+						</div>
+
+						<span>ADICIONAR AO CARRINHO</span>
+					</button>
+				</li>
+			))}
+		</ProductList>
+	);
 };
 
 export default Home;
